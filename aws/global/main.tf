e4d068314fdfd3425e216a4f309b53319f78642b
Aditya Wardianto <aditya.wardianto11@gmail.com>
2024-03-11 11:56:19 +0700

    feat: add s3 backend for tfstate

---
+++ aws/global/main.tf
@@ -0,0 +1,29 @@
+module "s3-tf-state" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.15.1"
+
+  bucket = "${local.project}-tf-state"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+}
+
+module "dynamodb-tf-state-lock" {
+  source  = "terraform-aws-modules/dynamodb-table/aws"
+  version = "4.0.1"
+
+  name           = "${local.project}-tf-state"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 3
+  write_capacity = 3
+
+  hash_key = "LockID"
+  attributes = [
+    {
+      name = "LockID"
+      type = "S"
+    }
+  ]
+
+}
